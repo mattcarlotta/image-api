736f5c712917d42bd21e9f86bfd633f58bf41054
Matt <carlotta.matt@gmail.com>
2021-09-14 16:54:27 -0700

    Add e2e integration tests

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ mod lrucache;
 mod reqimage;
 mod utils;
 
-fn main() {
+pub fn main() {
     let address = env::var("host").unwrap_or_else(|_| "127.0.0.1".to_string());
     let port = env::var("port").unwrap_or_else(|_| "5000".to_string());
     let host = format!("{}:{}", address, port);
@@ -39,5 +39,34 @@ fn main() {
             }),
             Err(e) => println!("Unable to handle request: {}", e),
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use reqwest::blocking::get;
+    use reqwest::header::{CONTENT_TYPE, TRANSFER_ENCODING};
+
+    #[test]
+    #[ignore]
+    fn e2e_integrations() {
+        let hello_res = get("http://localhost:5000").unwrap();
+        assert!(hello_res.status().is_success());
+        assert_eq!(
+            hello_res.headers().get(CONTENT_TYPE).unwrap(),
+            "text/html; charset=utf-8"
+        );
+
+        let notfound_res = get("http://localhost:5000/123").unwrap();
+        assert!(!notfound_res.status().is_success());
+        assert_eq!(
+            notfound_res.headers().get(CONTENT_TYPE).unwrap(),
+            "text/html; charset=utf-8"
+        );
+
+        let img_res = get("http://localhost:5000/placeholder.png").unwrap();
+        assert!(img_res.status().is_success());
+        assert_eq!(img_res.headers().get(CONTENT_TYPE).unwrap(), "image/png");
+        assert_eq!(img_res.headers().get(TRANSFER_ENCODING).unwrap(), "chunked");
     }
 }
